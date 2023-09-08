5816f603c578fa339b83ee1c5104cd3561c28cb1
RajPratapYadav <69375631+RajPratapYadav@users.noreply.github.com>
2023-09-08 18:14:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const productRoutes = require('./api/products'); // Import your product routes
 const app = express();
 const port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://localhost:27017/ecommerce', {
+mongoose.connect('mongodb+srv://r7905455:rh9GzGuLyhLsCLME@cluster0.c2ypzfg.mongodb.net/ecommerce', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
