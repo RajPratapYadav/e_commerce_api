913277b86de038a871b6c80a47a0463ae704562c
RajPratapYadav <69375631+RajPratapYadav@users.noreply.github.com>
2023-09-08 18:26:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,16 +5,31 @@ const productRoutes = require('./api/products'); // Import your product routes
 const app = express();
 const port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb+srv://r7905455:rh9GzGuLyhLsCLME@cluster0.c2ypzfg.mongodb.net/ecommerce', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+//mongoose.connect('mongodb+srv://r7905455:rh9GzGuLyhLsCLME@cluster0.c2ypzfg.mongodb.net/ecommerce', {
+  //useNewUrlParser: true,
+  //useUnifiedTopology: true,
+//});
 
 app.use(express.json());
 
 // Use the product routes
 app.use('/products', productRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+//app.listen(port, () => {
+  //console.log(`Server is running on port ${port}`);
+//});
+const connectToDb = async () => {
+   let MongoURL = "mongodb+srv://r7905455:rh9GzGuLyhLsCLME@cluster0.c2ypzfg.mongodb.net/ecommerce";
+   let options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+   };
+   try {
+      await mongoose.connect(MongoURL, options);
+      console.log(`Connected to mongo db--->${MongoURL}`);
+   } catch (error) {
+      console.log(error);
+      console.error("Could not connect to MongoDB");
+   }
+};
+app.listen(config.port, connectToDb(), console.log(`Starzopp server runing on ${config.port}`));
